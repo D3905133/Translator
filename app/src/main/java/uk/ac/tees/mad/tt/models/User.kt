fa1eaebb2cc406dda17eadbfb9806dc7069fd3e0
D3905133 <167678299+D3905133@users.noreply.github.com>
2024-12-20 10:29:24 +0000

    commit 16

--- app/src/main/java/uk/ac/tees/mad/tt/models/User.kt
+++
@@ -1,8 +0,0 @@
-package uk.ac.tees.mad.tt.models
-
-data class User(
-    val name : String = "",
-    val profilePicture : String = "",
-    val email : String = "",
-    val password : String = "",
-)
